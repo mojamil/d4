919e05de80dfcb4150de1ce038c531fa136eb9d7
Shin Bamba <sbamba@stuy.edu>
2019-04-06 22:28:39 -0400

    histogram

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 from flask import Flask,render_template
+from flask_cors import CORS
 app = Flask(__name__)
+cors = CORS(app)
 
 @app.route('/')
 def home():
@@ -15,6 +17,15 @@ def cereal():
     fields=fields[3:12]
     return render_template("cereal.html")
 
+@app.route('/histo')
+def histo():
+	data=open("data/cereal.csv", "r")
+	fields=data.readline().split(",")
+	# print(fields);
+	fields=fields[3:12]
+	# print(fields);
+	return render_template("histo.html",fields=fields)
+	
 if __name__ == '__main__':
     app.debug = True
     app.run()
--- static/bubble.js
+++ static/bubble.js
@@ -1,48 +1,125 @@
-var width = 960,
-    height = 500;
+// set the dimensions and margins of the graph
+var margin = {top: 20, right: 20, bottom: 30, left: 100},
+    width = 1000 - margin.left - margin.right,
+    height = 500 - margin.top - margin.bottom;
 
-var y = d3.scaleLinear()
-    .range([height, 0]);
+// set the ranges
+var x = d3.scaleLinear().range([0, width])
+          // .domain([new Date(2010, 6, 3), new Date(2012, 0, 1)])
+          // .rangeRound([0, width]);
+var y = d3.scaleLinear().range([height, 0]);
 
-var chart = d3.select(".chart")
-    .attr("width", width)
-    .attr("height", height);
+var active = "Calories"; 
 
-	
-d3.csv('cereal.csv')
+// number of bars created
+var bars = 10;
+// append the svg object to the body of the page
+// append a 'group' element to 'svg'
+// moves the 'group' element to the top left margin
+var svg = d3.select("body").append("svg")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
+  .append("g")
+    .attr("transform", 
+          "translate(" + margin.left + "," + margin.top + ")");
+// set the parameters for the histogram
+var histogram = d3.histogram()
+    .value(function(d) {return d[active]; })
+    // .domain(x.domain())
+    .thresholds(bars);
+// sets starting point of bar	
+var start = 0;
+// get the data
+d3.csv('/static/data/cereal.csv')
     .then(function(data){
 	data.forEach(function(d) {
-	    d["Protein"] = +d["Protein"];
+	d[active] = +d[active]
+	});
+	// group the data for the bars
+	var bins = histogram(data);
+	console.log(bins);
+  // Scale the range of the data in the y domain
+  y.domain([0, d3.max(bins, function(d) { return d.length; })]);
+console.log("yes")
+  // append the bar rectangles to the svg element
+  svg.selectAll("rect")
+	.data(bins)
+    .enter().append("rect")
+    .attr("x", function(d){
+		var old = start;
+		start+=width/ bars;
+		return old;
 	})
-	// y.domain([0, d3.max(data, function(d) { return d.value; })]);
-// d3.tsv("data.tsv", type, function(error, data) 
-  // y.domain([0, d3.max(data, function(d) { return d.value; })]);
+	.attr("transform", function(d) {
+		  return "translate(0"+"," + y(d.length) + ")"; })
+      .attr("width", function(d) {return width/ bars})
+      .attr("height", function(d) { return height - y(d.length); })
+	.attr('fill', 'blue');
 
-  var barWidth = width / data.length;
+    // .attr("fill", function(d) { return colorScale(d.y) });
+  // add the x Axis
+  svg.append("g")
+	.attr("class", "x-axis")
+    .attr("transform", "translate(0," + height + ")")
+    .call(d3.axisBottom(x));
+// add text to x-axis
+	svg.append("text")
+	    .attr("class", "labelx")
+	    .attr('id', 'xAxisLabel')
+	    .attr("x", width)
+	    .attr("y", height + margin.bottom)
+	    .style("text-anchor", "end")
+	    .text(active);
+  // add the y Axis
+  svg.append("g")
+	.attr("class", "y-axis")
+    .call(d3.axisLeft(y));
+      
+});
 
-  var bar = chart.selectAll("g")
-      .data(data)
-    .enter().append("g")
-      .attr("transform", function(d, i) { return "translate(" + i * barWidth + ",0)"; });
+function update(){
+	console.log(active);
+    d3.csv('/static/data/cereal.csv')
+	.then(function(data){
+	    data.forEach(function(d) {
+		d[active] = +d[active];
+		//alert( d.calories )
+		});
+		var bins = histogram(data);
+	    d3.select('.x-axis') // redraw the xAxis
+		.transition().call(d3.axisBottom(x))
+		d3.select('.y-axis') // redraw the yAxis
+		.transition().call(d3.axisLeft(y));
+		d3.select('#xAxisLabel')
+		.transition() // change the yAxisLabel
+		.text(active)
+	    d3.selectAll('rect') // move the bars
+		.data(bins)
+		.transition().attr("height", function(d) { return height - y(d.length); })
+		.attr("transform", function(d) {
+		  return "translate(0"+"," + y(d.length) + ")"; })
+		// .transition().duration(1000)
+		.delay(function (d,i) {;return i*100})
+		
+		// .attr('cx',xMap);
 
-  bar.append("rect")
-      .attr("y", function(d) { return y(d.value); })
-      .attr("height", function(d) { return height - y(d.value); })
-      .attr("width", barWidth - 1);
+	})
+    //Needs to be created
+};
 
-  bar.append("text")
-      .attr("x", barWidth / 2)
-      .attr("y", function(d) { return y(d.value) + 3; })
-      .attr("dy", ".75em")
-	.text(function(d) { return d.value; })});
-
-d3.csv('cereal.csv')
+var fields=NaN
+d3.csv('/static/data/cereal.csv')
     .then(function(data){
-	{
-	y.domain([0, d3.max(data, function(d) { return d.value; })])}});
-
-
-function type(d) {
-  d.value = +d.value; // coerce to number
-  return d;
-}
+	fields=Object.keys(data[0]).slice(3,12)
+	for (var i=0;i<fields.length;i++) {
+		// console.log("WORRRKINGGGG");
+	    (function(i){
+		document.getElementById(fields[i]).addEventListener('click',(e)=>{
+			// console.log("pushed")
+		    // console.log(e)
+		    active=fields[i]
+		    update()
+		});
+	    }(i))
+	}
+	});
